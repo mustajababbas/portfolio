2742f74b6d4206c11c06d4ad6504e8db42e0cf71
Syed Mustajab Abbas Nqvi <85496904+wawawasi@users.noreply.github.com>
2023-04-07 12:43:02 +0500

    new changes

--- js/main.js
+++ js/main.js
@@ -314,16 +314,6 @@ darkModeToggle.addEventListener('click', () => {
 
 
 
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card-wrapper")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
 
 
   setTimeout(function(){
